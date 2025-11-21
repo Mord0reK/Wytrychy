745ba5bd81fc87a5db2e3c25212b686dff17624f
Mord0reK <marcel.stosio51@gmail.com>
2025-11-21 12:00:45 +0100

    test

--- PHP - Obsluga bazy danych/index.php
+++ PHP - Obsluga bazy danych/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("localhost:33060", "root", "", "Wytrychy-21-11");
+$connection = mysqli_connect("localhost:13306", "root", "rootpassword", "Wytrychy-21-11");
 
 $zapytanie = mysqli_query($connection, "SELECT * FROM aktorzy");
 
