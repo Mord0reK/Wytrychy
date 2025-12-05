727f4d02d063e61963a45142808f655e1002389e
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2025-12-05 13:01:01 +0100

    Update restauracja.php

--- Egzaminy/Czerwiec 2018 - Restauracja/restauracja.php
+++ Egzaminy/Czerwiec 2018 - Restauracja/restauracja.php
@@ -10,3 +10,5 @@ $nr_tel=$_POST['telefon'];
 $con = mysqli_connect("wytrychy-db", "root", "rootpassword", "baza-restauracja");
 
 $zapytanie = mysqli_query($con, "INSERT INTO rezerwacje (nr_stolika, data_rez, liczba_osob, telefon) VALUES (5, '$data_rez', $osob, '$nr_tel')");
+
+mysqli_close($con)
